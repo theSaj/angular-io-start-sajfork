7554080029377fe10a4ae2fe774cfae14edb5e25
theSaj <saj@easternstorm.net>
2020-02-24 00:05:02 -0500

    1 file modified, 2 added

---
+++ src/app/app-products-alerts/app-products-alerts.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-app-products-alerts',
+  templateUrl: './app-products-alerts.component.html',
+  styleUrls: ['./app-products-alerts.component.css']
+})
+export class AppProductsAlertsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
