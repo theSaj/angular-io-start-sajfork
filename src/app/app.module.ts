a95941d24ea883f84f31e988d61e7fcb11cacbfd
theSaj <saj@easternstorm.net>
2020-02-24 00:40:21 -0500

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductListComponent } from './product-list/product-list.component';
-import { AppProductsAlertsComponent } from './app-products-alerts/app-products-alerts.component';
+import { AppProductAlertsComponent } from './app-product-alerts/app-product-alerts.component';
 
 @NgModule({
   imports: [
